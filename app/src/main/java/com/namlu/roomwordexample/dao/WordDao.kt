143cc4a073e7e25820c5ab9849564af26ae60705
Nam Lu <namlu1205@gmail.com>
2019-09-11 14:25:41 +1000

    Use LiveData to return a List<Word>

--- app/src/main/java/com/namlu/roomwordexample/dao/WordDao.kt
+++ app/src/main/java/com/namlu/roomwordexample/dao/WordDao.kt
@@ -1,5 +1,6 @@
 package com.namlu.roomwordexample.dao
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
@@ -9,7 +10,7 @@ import com.namlu.roomwordexample.entities.Word
 @Dao
 interface WordDao {
     @Query("SELECT * from word_table ORDER BY word ASC")
-    fun getAllWords(): List<Word>
+    fun getAllWords(): LiveData<List<Word>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(word: Word)
